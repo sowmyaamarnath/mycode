4e431653f2cbc688367914a6db67b6c04a4f2164
sowmyaamarnath <48669432+sowmyaamarnath@users.noreply.github.com>
2020-06-14 10:53:22 -0500

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,6 @@ package com.test
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("HI THERE!");
+		System.out.println("HI THERE! Welcome to java!");
 	}
 }
